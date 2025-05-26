61650a9375a6cd5a3c78622e3a041a4ee50df98f
Randy Gulak <randy@randygulak.com>
2025-05-26 12:33:15 -0700

    first draft of website

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const navLinks = document.getElementById("nav-links");
+
+  if (hamburgerButton && navLinks) {
+    hamburgerButton.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+      hamburgerButton.classList.toggle("active");
+    });
+  }
+});
